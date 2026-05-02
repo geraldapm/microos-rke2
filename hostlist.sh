a23c30371d7895b516f6772021a9111175ef6959
Gerald Mahayadnya <geraldiaditya1111@gmail.com>
2026-05-02 14:41:15 +0700

    fix(hostlist): return hostlist member

--- hostlist.sh
+++ hostlist.sh
@@ -14,10 +14,9 @@ IP_FLOATING="$(echo $IP_SUBNET | cut -d. -f1-3).99"
 ### Ensure that the hostname has "wontrol" and "worker" inside for node role filtering
 hostlist=$(cat <<EOF
 192.168.122.101     gpmcontrolplane1
+192.168.122.102     gpmcontrolplane2
+192.168.122.103     gpmcontrolplane3
+192.168.122.104     gpmworker1    
+192.168.122.105     gpmworker2
 EOF
 )
-
-# 192.168.122.102     gpmcontrolplane2
-# 192.168.122.103     gpmcontrolplane3
-# 192.168.122.104     gpmworker1    
-# 192.168.122.105     gpmworker2
